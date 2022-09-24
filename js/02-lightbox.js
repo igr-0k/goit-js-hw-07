519725c120e9e8f7baa79827deb928b1de66df10
igr-0k <igor.01101110@gmail.com>
2022-09-24 12:48:48 +0300

    Task#2 - done!

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let itemsGallery = galleryItems;
+
+const galleryTemplate = itemsGallery
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt= "${description}" title = "${description}"/>
+    </a>`
+  )
+  .join('');
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('beforeend', galleryTemplate);
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
